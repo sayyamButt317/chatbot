356812126952cc21cdea01363366d543af11079e
IT <sayyambutt317@gmail.com>
2024-01-26 05:22:48 +0500

    updated app.py file

--- app.py
+++ app.py
@@ -1,27 +1,12 @@
-from flask import Flask, render_template, request
-from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
+import nltk
+#nltk.download('punkt')
+from nltk.steam.porter import PorterStemmer
+stemmer = PorterStemmer()
+def tokenize(sentance):
+    return nltk.word_tokenize(sentance)
+    
+def stem(word):
+    return stemmer.stem(word.lower())
 
-app = Flask(__name__)
-
-# Create a chatbot instance
-chatbot = ChatBot('TravelBot')
-
-# Create a new trainer for the chatbot
-trainer = ChatterBotCorpusTrainer(chatbot)
-
-# Train the chatbot on English language data
-trainer.train('chatterbot.corpus.english')
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/get_response', methods=['POST'])
-def get_response():
-    user_message = request.form['user_message']
-    response = chatbot.get_response(user_message)
-    return str(response)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+def bag_of_words(tokenized_sentence,all_words):
+    pass
