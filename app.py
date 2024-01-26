f7bfb73b33aa031f54c7419eb5f3750773559632
IT <sayyambutt317@gmail.com>
2024-01-26 05:30:12 +0500

    updated

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import nltk
 #nltk.download('punkt')
-from nltk.steam.porter import PorterStemmer
+from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
 def tokenize(sentance):
     return nltk.word_tokenize(sentance)
@@ -10,3 +10,8 @@ def stem(word):
 
 def bag_of_words(tokenized_sentence,all_words):
     pass
+
+a="Most Suitable travel location"
+print(a)
+a = tokenize(a)
+print(a)
